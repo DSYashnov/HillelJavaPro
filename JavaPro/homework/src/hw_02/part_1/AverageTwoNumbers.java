1327830b4fc4b132f2d75b7e98be5b44efd70049
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-12 22:54:37 +0300

    les2

---
+++ JavaPro/homework/src/hw_02/part_1/AverageTwoNumbers.java
@@ -0,0 +1,8 @@
+package hw_02.part_1;
+
+public class AverageTwoNumbers {
+    public static void main(String[] args) {
+        double average = Double.valueOf(args[0]) + Double.valueOf(args[1]);
+        System.out.println("Average is " + average/2);
+    }
+}
