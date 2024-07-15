9b630e71126d0ea9ff452342f08f85961c0afd54
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-15 20:28:57 +0300

    les2

---
+++ JavaPro/lecture/src/les_03/p1/A.java
@@ -0,0 +1,11 @@
+package les_03.p1;
+
+public class A {
+    private void m1(){} // private
+
+    void m2(){} // default
+
+    protected void m3(){} //  protected
+
+    public void m4(){} // public
+}
