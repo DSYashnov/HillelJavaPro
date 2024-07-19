f5e408fdd54b7ebfa4d223bf6e8b997bdb7df3ad
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-20 00:42:50 +0300

    les2

--- JavaPro/lecture/src/les_03/p1/B.java
+++ JavaPro/lecture/src/les_03/p1/B.java
@@ -4,9 +4,10 @@ public class B  extends A {
 
     @Override
     void m2(){super.m2();}
+
     @Override
     protected void m3(){super.m3();}
+
     @Override
     public void m4(){super.m4();}
-
 }
