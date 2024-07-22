cd4113bbf8fbabab3db65ed0f1ca1e481edb5b89
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-22 20:46:52 +0300

    les2

---
+++ JavaPro/homework/src/hw_03/task_06/FirstNumbers.java
@@ -0,0 +1,16 @@
+package hw_03.task_06;
+
+public class FirstNumbers {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 5;
+        for (int j = 0; j < 10; j++) {
+            int i = a - 5;
+            b += i;
+            System.out.println(b);
+        }
+    }
+}
+
+
+
