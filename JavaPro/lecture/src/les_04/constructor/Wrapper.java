180b595448ee0c88f43fe3615ea048ba14dec2c5
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-25 00:04:36 +0300

    les2

---
+++ JavaPro/lecture/src/les_04/constructor/Wrapper.java
@@ -0,0 +1,20 @@
+package les_04.constructor;
+
+public class Wrapper {
+        Integer i;
+        int in;
+
+        Boolean b;
+        boolean bool;
+
+    @Override
+    public String toString() {
+        return "Wrapper{" +
+                "i=" + i +
+                ", in=" + in +
+                ", b=" + b +
+                ", bool=" + bool +
+                '}';
+    }
+}
+
