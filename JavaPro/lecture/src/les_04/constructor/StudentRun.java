dfc5cb0165218d00d00b226e66b1aeb9cb699e88
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-25 03:03:09 +0300

    les2

---
+++ JavaPro/lecture/src/les_04/constructor/StudentRun.java
@@ -0,0 +1,15 @@
+package les_04.constructor;
+
+public class StudentRun {
+    public static void main(String[] args) {
+        Student st = new Student();
+        System.out.println(st);
+        st.setFirstName("Stanislav");
+        st.setLastName("Yashnov");
+        st.setAge(29);
+        System.out.println(st);
+
+        Student stud = new Student("Stas", "Yashnov");
+        System.out.println(stud);
+    }
+}
