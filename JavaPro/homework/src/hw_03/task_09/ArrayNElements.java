0d9c1cec0a26527198983a0e88189e9284627472
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-26 22:31:05 +0300

    les2

--- JavaPro/homework/src/hw_03/task_09/ArrayNElements.java
+++ JavaPro/homework/src/hw_03/task_09/ArrayNElements.java
@@ -1,11 +1,12 @@
 package hw_03.task_09;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayNElements {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("enter length arrays: ");
+        System.out.print("enter length arrays: ");
         int number = s.nextInt();
 
         int[] arr = new int[number];
@@ -14,7 +15,16 @@ public class ArrayNElements {
             double rand = Math.random() * number;
             arr[i] = (int) rand;
             System.out.print(arr[i] + " ");
+        }
+        Arrays.sort(arr);
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[0] < min) {
+                min = arr[0];
             }
         }
+        System.out.println();
+        System.out.println("minimal value: " + min);
     }
+}
 
