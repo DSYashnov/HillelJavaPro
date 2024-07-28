48d07e4bf344e9f60ad1f642c1a7f76af180b469
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-29 00:31:39 +0300

    les2

--- JavaPro/homework/src/hw_03/task_04/DoWhileExample.java
+++ JavaPro/homework/src/hw_03/task_04/DoWhileExample.java
@@ -1,4 +1,33 @@
 package hw_03.task_04;
 
 public class DoWhileExample {
-}
+    public static void main(String[] args) {
+
+        int d = 0;
+        do {
+            d++;
+            if (d % 2 != 0) {
+                System.out.print(d);
+            }
+        } while (d < 100);
+
+        System.out.println();
+        System.out.println("-------------");
+        System.out.println(factorial(10));
+    }
+
+        public static String factorial (int n) {
+            int j = 1;
+            int fact = 1;
+            do {
+                ++j;
+                fact *= j;
+            } while (j < n);
+            String s = "factorial number " + n + " = " + fact;
+            return s;
+        }
+    }
+
+
+
+
