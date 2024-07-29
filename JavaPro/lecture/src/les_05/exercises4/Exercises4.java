deba996fb738e51cf76a53e6c99edb44ed2e123b
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-29 22:50:19 +0300

    les2

--- JavaPro/lecture/src/les_05/exercises4/Exercises4.java
+++ JavaPro/lecture/src/les_05/exercises4/Exercises4.java
@@ -1,4 +1,20 @@
 package les_05.exercises4;
 
 public class Exercises4 {
+    static int method(int i){
+        return method2 (i *= 11);
+    }
+    static int method2 (int i){
+        return method3(i /= 11);
+    }
+    static int method3(int i){
+        return method4(i -= 11);
+    }
+    static int method4 (int i){
+        return i += 11;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(method(11));
+    }
 }
