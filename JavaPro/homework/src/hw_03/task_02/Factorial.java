2cdad6fba695a41f29b4a3ae953302154643befa
Stanislav Yashnov <s.yashnov@gmail.com>
2024-07-29 22:50:23 +0300

    les2

--- JavaPro/homework/src/hw_03/task_02/Factorial.java
+++ JavaPro/homework/src/hw_03/task_02/Factorial.java
@@ -8,6 +8,7 @@ public class Factorial {
         int n = sc.nextInt();
         System.out.println(factorial(n));
     }
+
     static int factorial(int n) {
         int fact = 1;
         for (int i = 1; i <= n; i++) {
