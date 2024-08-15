6e85ef5246bf60a5428f214ef4f469921783fe8d
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-15 21:57:40 +0300

    les2

--- JavaPro/homework/src/hw_05/Animals/Animal.java
+++ JavaPro/homework/src/hw_05/Animals/Animal.java
@@ -1,8 +1,14 @@
 package hw_05.Animals;
 
 public class Animal {
-
+    private static int count =0;
     private String name;
+
+    public Animal(String name) {
+        this.name = name;
+        count++;
+        System.out.println("Створено тварин - " + count);
+    }
 
     public String getName() {
         return name;
@@ -14,5 +20,8 @@ public class Animal {
 
     public void run (int range){
         System.out.println( name + " пробіг " + range + "м");
+    }
+    public void swim(int range){
+        System.out.println(name + " проплив " + range + "м");
     }
 }
