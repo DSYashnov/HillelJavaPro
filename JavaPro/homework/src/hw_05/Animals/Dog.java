743481393a81a920e06ba711b881767b8d716552
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-16 23:57:29 +0300

    les2

--- JavaPro/homework/src/hw_05/Animals/Dog.java
+++ JavaPro/homework/src/hw_05/Animals/Dog.java
@@ -2,12 +2,24 @@ package hw_05.Animals;
 
 public class Dog extends Animal {
 
+    Dog (String name) {
+        super(name);
+    }
     public void run(int range) {
         if (range > 500) {
             System.out.println("Собака не може подолати таку дистанцію");
         } else {
-            System.out.println(getName() + " пробіг " + range + "м");
+            System.out.println(getName() + " пробіг " + range + " м ");
 
+        }
+    }
+
+    @Override
+    public void swim(int range) {
+        if (range > 10) {
+            System.out.println("Собака не може пропливти таку дистанцію");
+        } else {
+            System.out.println(getName() +  " проплив " + range + " м ");
         }
     }
 }
