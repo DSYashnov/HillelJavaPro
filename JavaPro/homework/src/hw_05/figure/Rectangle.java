f436f935e42fc55b3ce93bcacae660817ac529f8
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-18 23:34:25 +0300

    les2

--- JavaPro/homework/src/hw_05/figure/Rectangle.java
+++ JavaPro/homework/src/hw_05/figure/Rectangle.java
@@ -3,14 +3,26 @@ package hw_05.figure;
 import java.util.Scanner;
 
 public class Rectangle implements SquareEx{
+    private double length;
+    private double width;
+    private double square;
+
+    public Rectangle() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first side of rectangle: ");
+        this.length = s.nextDouble();
+        System.out.print("Enter second side of rectangle: ");
+        this.width = s.nextDouble();
+        this.square = square();
+    }
+
     @Override
     public double square() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter first side cube: ");
-        double a = s.nextDouble();
-        System.out.print("Enter second side cube: ");
-        double b = s.nextDouble();
-        double square = a * b;
-        return square;
+        return length * width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle with sides: " + length + " and " + width + " has area: " + square;
     }
 }
