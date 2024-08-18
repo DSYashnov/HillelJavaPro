c9093625c9824a6829ecd627ad1b2fa693ab6f23
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-18 23:34:21 +0300

    les2

--- JavaPro/homework/src/hw_05/figure/Circle.java
+++ JavaPro/homework/src/hw_05/figure/Circle.java
@@ -3,13 +3,24 @@ package hw_05.figure;
 import java.util.Scanner;
 
 public class Circle implements SquareEx{
+    private double radius;
+    private double square;
+
+    public Circle() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter radius in circle: ");
+        this.radius = s.nextDouble();
+        this.square = square();
+    }
+
     @Override
     public double square() {
         final double PI = 3.14;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter radius in circle: ");
-        double r = s.nextDouble();
-        double squareCircle = PI*(r*r);
-        return squareCircle;
+        return PI * (radius * radius);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle with radius: " + radius + " has area: " + square;
     }
 }
