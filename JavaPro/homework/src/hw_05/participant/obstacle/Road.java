7b3ebd887af7d2ad7b81742f4df236c1683e27de
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-20 21:13:45 +0300

    les2

---
+++ JavaPro/homework/src/hw_05/participant/obstacle/Road.java
@@ -0,0 +1,13 @@
+package hw_05.participant.obstacle;
+
+public class Road extends  Obstacle{
+    private int lenghts;
+
+    public Road(int height) {
+        this.lenghts = lenghts;
+    }
+
+    public int getHeight() {
+        return lenghts;
+    }
+}
