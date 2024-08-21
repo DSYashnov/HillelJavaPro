5896122c0b6534e7b874430862f26ac5a061bbb4
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-21 20:42:50 +0300

    les2

---
+++ JavaPro/homework/src/hw_05/participant/obstacle/Wall.java
@@ -0,0 +1,13 @@
+package hw_05.participant.obstacle;
+
+public class Wall extends Obstacle {
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
