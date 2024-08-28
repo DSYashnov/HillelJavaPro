73df901e6ee63975a00b1575f85e88c911640237
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-28 18:56:44 +0300

    les2

--- JavaPro/homework/src/hw_05/participant/service/GameService.java
+++ JavaPro/homework/src/hw_05/participant/service/GameService.java
@@ -1,13 +1,39 @@
 package hw_05.participant.service;
 
 import hw_05.participant.Participant;
+import hw_05.participant.obstacle.Obstacle;
+import hw_05.participant.obstacle.Road;
+import hw_05.participant.obstacle.Wall;
 
 public class GameService {
     public void run(int userNumber, int obstacleNumber){
         Generator gen = new Generator();
-        Participant[] participants = gen.generateParticipant(15);
+        Participant[] participants = gen.generateParticipant(userNumber);
+        Obstacle[] obstacles = gen.generateObstacle(obstacleNumber);
+
         for (Participant p : participants){
             System.out.println(p);
+            for (Obstacle obs : obstacles){
+                System.out.println(">>>>>>>" + obs);
+                if(obs instanceof Wall){
+                    int jump = p.getJump();
+                    if (jump < ((Wall)obs).getHeight()){
+                        System.out.println("fail");
+                        break;
+                    } else {
+                        System.out.println("next...");
+                    }
+                } else if (obs instanceof Road) {
+                    int run = p.getRun();
+                    if (run < ((Road)obs).getLenghts()){
+                        System.out.println("fail");
+                        break;
+
+                } else {
+                    System.out.println("next...");
+                }
+                }
+            }
         }
     }
 }
