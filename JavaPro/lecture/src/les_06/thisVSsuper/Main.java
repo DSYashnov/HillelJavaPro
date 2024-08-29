be35d48f830a755ce8b7df9d52bc83c4ee49b089
Stanislav Yashnov <s.yashnov@gmail.com>
2024-08-29 21:53:51 +0300

    les2

---
+++ JavaPro/lecture/src/les_06/thisVSsuper/Main.java
@@ -0,0 +1,20 @@
+package les_06.thisVSsuper;
+
+public class Main {
+    public static void main(String[] args) {
+        Parent p1 = new Parent();
+        System.out.println(p1);
+        System.out.println("-------------------");
+        Parent p2 = new Parent("Stas", "Yashnov");
+        System.out.println(p2);
+        System.out.println("-------------------");
+        Child ch1 = new Child(12);
+        System.out.println(ch1);
+        System.out.println("-------------------");
+        Child ch2 = new Child("Stas", "Yashnov", "Stas", "Yashnov");
+        System.out.println(ch2);
+        System.out.println("-------------------");
+        Child ch3 = new Child("Stas", "Yashnov");
+        System.out.println(ch3);
+    }
+}
