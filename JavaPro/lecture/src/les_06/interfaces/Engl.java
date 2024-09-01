22dda5228024c75570da292135383829c47fa639
Stanislav Yashnov <s.yashnov@gmail.com>
2024-09-01 20:00:51 +0300

    les2

---
+++ JavaPro/lecture/src/les_06/interfaces/Engl.java
@@ -0,0 +1,13 @@
+package les_06.interfaces;
+
+public class Engl implements Say{
+    @Override
+    public void sayHello() {
+        System.out.println("Say hello....");
+    }
+
+    @Override
+    public void sayGoodBuy() {
+        System.out.println("Goodbuy...");
+    }
+}
