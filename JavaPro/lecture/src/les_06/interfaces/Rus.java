f94e2f00354040e95c0cfba2654658c60716e20c
Stanislav Yashnov <s.yashnov@gmail.com>
2024-09-01 20:00:58 +0300

    les2

---
+++ JavaPro/lecture/src/les_06/interfaces/Rus.java
@@ -0,0 +1,13 @@
+package les_06.interfaces;
+
+public class Rus implements Say{
+    @Override
+    public void sayHello() {
+        System.out.println("Привет....");
+    }
+
+    @Override
+    public void sayGoodBuy() {
+        System.out.println("Пока....");
+    }
+}
