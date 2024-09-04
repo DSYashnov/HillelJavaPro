eb1408dbecf7b85f8b70b115db07a5fb35d60c62
Stanislav Yashnov <s.yashnov@gmail.com>
2024-09-04 23:12:01 +0300

    les2

---
+++ JavaPro/lecture/src/les_06/interfaces/MainLang.java
@@ -0,0 +1,16 @@
+package les_06.interfaces;
+
+public class MainLang {
+    public static void main(String[] args) {
+        Say say;
+
+        if (args.length != 0 && args[0].equals("rus")){
+            say = new Rus();
+        }else{
+            say = new Engl();
+        }
+        say.sayHello();
+        say.sayGoodBuy();
+        Say.hello();
+    }
+}
