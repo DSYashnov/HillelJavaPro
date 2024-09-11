6e9ac7fd9a1d71ff4f006e189780ed9d7805dd3c
Stanislav Yashnov <s.yashnov@gmail.com>
2024-09-11 23:36:55 +0300

    les2

---
+++ JavaPro/lecture/src/les_07/ex/Ex3.java
@@ -0,0 +1,16 @@
+package les_07.ex;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        System.out.println(foo.a);
+        System.out.println(foo.b);
+        System.out.println(foo.c);
+        }
+    }
+    class Foo {
+        int a = 5;
+        protected int b = 6;
+        public int c = 7;
+    }
+
