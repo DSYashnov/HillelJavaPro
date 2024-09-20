a5294b8bbf788391debef4002465d37bfcb28435
Stanislav Yashnov <s.yashnov@gmail.com>
2024-09-20 23:27:21 +0300

    les2

---
+++ JavaPro/lecture/src/les_07/enumerator/Enumerator1.java
@@ -0,0 +1,45 @@
+package les_07.enumerator;
+
+public class Enumerator1 {
+    private String email;
+    private Enumerator status;
+    private boolean isActive;
+
+    public Enumerator1(String email) {
+        this.email = email;
+        this.status = Enumerator.PENDING;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Enumerator getStatus() {
+        return status;
+    }
+
+    public void setStatus(Enumerator status) {
+        this.status = status;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "EnumeratorRun{" +
+                "email='" + email + '\'' +
+                ", status=" + status +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
