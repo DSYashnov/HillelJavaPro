7e4cb8f1b2c6488a422fd132ace9612d34ae640d
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-07 23:59:30 +0300

    les2

---
+++ JavaPro/homework/src/hw_08/Collection.java
@@ -0,0 +1,13 @@
+package hw_08;
+
+public interface Collection {
+    boolean add(String o);
+    boolean add(int index, String o);
+    boolean delete (String o);
+    String get(int index);
+    boolean contain(String o);
+    boolean equals (Collection collection);
+    boolean clear();
+    int size();
+}
+
