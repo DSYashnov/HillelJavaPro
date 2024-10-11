178b2d2d343151d64ba91f5049ebb0c676adbc79
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-11 23:37:05 +0300

    les2

---
+++ JavaPro/lecture/src/les_08/set/TreeSetEx.java
@@ -0,0 +1,37 @@
+package les_08.set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetEx {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet();
+
+        set.add(10);
+        set.add(5);
+        set.add(6);
+        set.add(7);
+        set.add(8);
+        set.add(3);
+        set.add(2);
+        set.add(9);
+        set.add(0);
+        set.add(1);
+
+        System.out.println(set);
+        set.add(11);
+        System.out.println(set);
+
+
+        TreeSet<String> ts = new TreeSet<>();
+
+        ts.add("One");
+        ts.add("one");
+        ts.add("tow");
+        ts.add("Tne");
+        ts.add("ane");
+        ts.add("Bne");
+
+        System.out.println(ts);
+    }
+}
