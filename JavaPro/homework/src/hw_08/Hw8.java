467ff0598816f12c9d46294bee03b8e55d90e834
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-13 23:36:26 +0300

    les2

--- JavaPro/homework/src/hw_08/Hw8.java
+++ JavaPro/homework/src/hw_08/Hw8.java
@@ -1,7 +1,12 @@
 package hw_08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hw8 {
     public static void main(String[] args) {
+        List<String> list = new ArrayList();
+        list.add("1");
 
     }
 }
