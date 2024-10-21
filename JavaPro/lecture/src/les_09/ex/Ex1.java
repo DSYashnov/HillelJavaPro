4895a2439fd1de50f8b2cd1c7dd2091acb4b1350
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-21 23:17:03 +0300

    les2

---
+++ JavaPro/lecture/src/les_09/ex/Ex1.java
@@ -0,0 +1,20 @@
+package les_09.ex;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Days d1 = Days.TH;
+        Days d2 = Days.M;
+
+        for (Days d : Days.values()) {
+            if (d.equals(Days.F)) break;
+            d2 = d;
+        }
+        System.out.println((d1==d2) ? "same" : "not same");
+        Days a = Days.S;
+        Days a1 = Days.S;
+
+        System.out.println(a == a1);
+        System.out.println(a.equals(a1));
+    }
+    enum Days {M, T, W, TH, F, SA, S}
+}
