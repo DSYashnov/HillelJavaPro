1c5b48c00aa918d3adf6527b6652ec77d6a10368
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-27 23:53:35 +0200

    les2

--- JavaPro/lecture/src/les_09/collection/ArrayCopy.java
+++ JavaPro/lecture/src/les_09/collection/ArrayCopy.java
@@ -4,7 +4,7 @@ public class ArrayCopy {
     public static void main(String[] args) {
         int index = 2;
 
-        String [] str1 = new String[6];
+        String[] str1 = new String[6];
 
         str1[0] = "1";
         str1[1] = "2";
@@ -12,22 +12,40 @@ public class ArrayCopy {
         str1[3] = "4";
         str1[4] = "5";
 
-        for(String s : str1) System.out.print(s + " ");
+        for (String s : str1) {
+            System.out.print(s + " ");
+        }
+        String[] str2 = new String[10];
 
-        String [] str2 = new String[6];
-
-        System.arraycopy(str1, 0, str1, 0, str1.length -1 - index);
-
+        System.arraycopy(str1, 0, str2, 0, str1.length);
         System.out.println();
+        for (String s : str2) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+
+        System.arraycopy(str2, index, str2, index + 1, str1.length - index);
+        System.out.println();
+        for (String s : str2) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+
+        str2[index] = "10";
         for (String s : str2) System.out.print(s + " ");
-        System.out.println();
-        str1[index] = "10";
-        for (String s : str1) System.out.print(s + " ");
 
         int numMoved = str1.length - 1 - index;
 
-        System.arraycopy(str1, index + 1, str1, index, numMoved);
-        str1[str1.length - 1] = null;
+        System.arraycopy(str2, index + 1, str2, index, numMoved);
         System.out.println();
+        for (String s : str2) {
+            System.out.print(s + " ");
+        }
+
+        str2[5] = null;
+        System.out.println();
+        for (String s : str2) {
+            System.out.print(s + " ");
+        }
     }
 }
