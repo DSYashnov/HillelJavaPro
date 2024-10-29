32f349df80569a882c9f0bfe4d4cdec6b6387cf4
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-29 22:50:17 +0200

    les2

---
+++ JavaPro/lecture/src/les_09/collection/ListOf.java
@@ -0,0 +1,11 @@
+package les_09.collection;
+
+import java.util.List;
+
+public class ListOf {
+    public static void main(String[] args) {
+        List<String> str = List.of("1", "2");
+
+        System.out.println(str.getClass().getSimpleName());
+    }
+}
