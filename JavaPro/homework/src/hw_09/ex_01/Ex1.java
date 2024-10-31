1fb858cfbc164e8934ebec07878252a208c8d3e5
Stanislav Yashnov <s.yashnov@gmail.com>
2024-10-31 22:30:24 +0200

    les2

---
+++ JavaPro/homework/src/hw_09/ex_01/Ex1.java
@@ -0,0 +1,14 @@
+package hw_09.ex_01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+    }
+    public static void countOccurance(ArrayList ar, String str) {
+
+    }
+
+}
