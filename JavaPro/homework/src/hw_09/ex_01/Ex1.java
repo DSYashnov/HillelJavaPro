a3269b8cf81d1a39638e4a7dcf99f356d403115e
Stanislav Yashnov <s.yashnov@gmail.com>
2024-11-05 01:08:02 +0200

    les2

--- JavaPro/homework/src/hw_09/ex_01/Ex1.java
+++ JavaPro/homework/src/hw_09/ex_01/Ex1.java
@@ -1,14 +1,35 @@
 package hw_09.ex_01;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Ex1 {
     public static void main(String[] args) {
-
+        ArrayList<String> list  = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("b");
+        list.add("n");
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("b");
+        list.add("n");
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("b");
+        list.add("n");
+        System.out.println(countOccurrence(list, "b"));
     }
-    public static void countOccurance(ArrayList ar, String str) {
-
+    public static int countOccurrence(ArrayList<String> ar, String str) {
+        int count = 0;
+        for (String string : ar){
+            if (string.equals(str)) count++;
+        }
+        return count;
     }
-
 }
