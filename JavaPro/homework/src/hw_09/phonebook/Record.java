5aacd64c98f5fa0f539399d069ad6334ef641427
Stanislav Yashnov <s.yashnov@gmail.com>
2024-11-10 00:59:35 +0200

    les2

---
+++ JavaPro/homework/src/hw_09/phonebook/Record.java
@@ -0,0 +1,23 @@
+package hw_09.phonebook;
+
+public class Record {
+    private String name;
+    private String phone;
+
+    public Record(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
