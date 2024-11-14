1307d580fa165172b760afe116c49a9baee57a0d
Stanislav Yashnov <s.yashnov@gmail.com>
2024-11-14 21:20:40 +0200

    les11

---
+++ JavaPro/lecture/src/les_11/exception/CloseExample.java
@@ -0,0 +1,13 @@
+package les_11.exception;
+
+import java.sql.SQLException;
+
+public class CloseExample {
+    public static void main(String[] args) throws SQLException {
+        try (Test text = new Test();){
+            text.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
