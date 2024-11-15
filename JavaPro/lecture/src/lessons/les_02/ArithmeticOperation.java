9d608f2710a35f1822de577fedd72f80c04fa19d
Stanislav Yashnov <s.yashnov@gmail.com>
2024-11-15 02:14:48 +0200

    les11

---
+++ JavaPro/lecture/src/lessons/les_02/ArithmeticOperation.java
@@ -0,0 +1,12 @@
+package lessons.les_02;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        double a = 2d;
+        double b = 3d;
+        double c = 4d;
+        double d = 6d;
+
+        System.out.println(a + b / c * d - a);
+    }
+}
