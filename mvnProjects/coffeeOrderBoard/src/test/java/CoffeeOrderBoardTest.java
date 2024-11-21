bd6d6d6ce26e465993d4f06d451ae7c4a9822f26
Stanislav Yashnov <s.yashnov@gmail.com>
2024-11-21 22:51:26 +0200

    les11

---
+++ mvnProjects/coffeeOrderBoard/src/test/java/CoffeeOrderBoardTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoffeeOrderBoardTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void deliver() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testDeliver() {
+    }
+
+    @Test
+    void draw() {
+    }
+}
