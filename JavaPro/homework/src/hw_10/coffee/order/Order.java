6e1d947f0e932102db40bccf0c8f21c0f9b5705c
Stanislav Yashnov <s.yashnov@gmail.com>
2024-11-27 23:57:11 +0200

    les11

---
+++ JavaPro/homework/src/hw_10/coffee/order/Order.java
@@ -0,0 +1,24 @@
+package hw_10.coffee.order;
+
+public class Order {
+    private int number;
+    private String name;
+
+    public Order(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Order #" + number + ": " + name;
+    }
+}
