e3267b8149eaded94ae990b18f8aee721b4df59b
Stanislav Yashnov <s.yashnov@gmail.com>
2024-12-01 18:44:36 +0200

    les11

---
+++ JavaPro/lecture/src/lessons/les_11/lombok/User.java
@@ -0,0 +1,15 @@
+package lessons.les_11.lombok;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private String name;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private String email;
+    private String phone;
+}
