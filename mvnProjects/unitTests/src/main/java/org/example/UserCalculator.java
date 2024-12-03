4fd838a5cdc3f42e475d3bfbecb63d36d06e7cab
Stanislav Yashnov <s.yashnov@gmail.com>
2024-12-03 23:15:20 +0200

    les11

---
+++ mvnProjects/unitTests/src/main/java/org/example/UserCalculator.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class UserCalculator {
+
+    public String calculatorFullName (String name) {
+        System.out.println("run calculateFullName");
+        return name + " " + name;
+    }
+}
