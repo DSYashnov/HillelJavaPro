0565e5c1f75634312bf6af6218f4e2cba76044bd
Stanislav Yashnov <s.yashnov@gmail.com>
2025-03-21 00:12:16 +0200

    les11

---
+++ JavaPro/lecture/src/lessons/les_15/ChangeWordPlace.java
@@ -0,0 +1,19 @@
+package lessons.les_15;
+
+public class ChangeWordPlace {
+    private static String EX = "1 2 3 4. 2 3 4. 3 4 5. 6 7. 1";
+    private static String REZ = "4 2 3 1. 4 3 2. 5 4 3. 7 6. 1";
+
+    public static void main(String[] args) {
+        assert changeWordOrder(EX).equals(REZ);
+    }
+
+    private static String changeWordOrder(String text){
+        return null;
+    }
+    private static String swap (String sent) {
+        return null;
+    }
+
+}
+
