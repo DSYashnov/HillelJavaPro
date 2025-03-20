f84c726fc95077b28808eb32e01562ce5d5991dd
Stanislav Yashnov <s.yashnov@gmail.com>
2025-03-21 00:12:11 +0200

    les11

---
+++ JavaPro/lecture/src/lessons/les_15/ChangeWordlnText.java
@@ -0,0 +1,9 @@
+package lessons.les_15;
+
+public class ChangeWordlnText {
+    static String TEXT = "test_word";
+    static String TEXT_EXAMPLE = "test word 231 3465435 345346 234 1245 3456 12345243 q345234";
+    public static void main(String[] args) {
+        int length = 4;
+    }
+}
