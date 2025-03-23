2c3cae8e7e89f4ddc1332bd78b0f5feff9047643
Stanislav Yashnov <s.yashnov@gmail.com>
2025-03-24 01:45:50 +0200

    les11

---
+++ JavaPro/lecture/src/lessons/les_16/filestream/FileOutputStreamEx.java
@@ -0,0 +1,11 @@
+package lessons.les_16.filestream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FileOutputStreamEx {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
