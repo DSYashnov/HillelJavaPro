3156644add92f87f9fb5d16f2b91fd523094e6f5
Stanislav Yashnov <s.yashnov@gmail.com>
2025-03-25 00:51:18 +0200

    les11

---
+++ JavaPro/lecture/src/lessons/les_16/folders/FolderSeparator.java
@@ -0,0 +1,15 @@
+package lessons.les_16.folders;
+
+import java.io.File;
+
+public class FolderSeparator {
+    public static void main(String[] args) {
+        String folder = "baseFolder";
+        //dir1/dir2/ -linux
+        //dir1\dir2\
+
+        folder = folder.concat(File.separator).concat("1").concat(File.separator).concat("1").concat(File.separator).concat("1");
+
+        System.out.println(folder);
+    }
+}
