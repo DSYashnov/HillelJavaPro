2e1956f762e3e8e067818cafbb36db4ba4e86273
Stanislav Yashnov <s.yashnov@gmail.com>
2025-03-28 03:01:55 +0200

    les11

---
+++ JavaPro/homework/src/les_16/Main.java
@@ -0,0 +1,21 @@
+package les_16;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Load configuration from a file
+            FileLoggerConfiguration config = FileLoggerConfigurationLoader.load("config.txt");
+
+            // Create a logger
+            FileLogger logger = new FileLogger(config);
+
+            // Log messages
+            logger.info("This is an info message.");
+            logger.debug("This is a debug message.");
+        } catch (IOException | FileMaxSizeReachedException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
