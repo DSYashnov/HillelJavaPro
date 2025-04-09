84fc9b40cec0ae4decf57ab6a0e66d13df3aaf8c
Stanislav Yashnov <s.yashnov@gmail.com>
2025-04-09 23:46:34 +0300

    les11

---
+++ mvnProjects/blackjack/src/main/java/com/hillel/blackjack/dto/Suit.java
@@ -0,0 +1,17 @@
+package com.hillel.blackjack.dto;
+
+public enum Suit {
+    Hearts ('♥'),
+    Clubs('♣'),
+    Spades('♠'),
+    Diamonds('♦');
+
+    private final char symbol;
+
+    public char getSymbol() {
+        return symbol;
+    }
+    Suit (char symbol) {
+        this.symbol = symbol;
+    }
+}
