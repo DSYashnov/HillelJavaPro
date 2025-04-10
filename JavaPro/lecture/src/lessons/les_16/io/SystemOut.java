9d33e84645521ea56152a8dc937245d046885f33
Stanislav Yashnov <s.yashnov@gmail.com>
2025-04-10 23:28:15 +0300

    les11

---
+++ JavaPro/lecture/src/lessons/les_16/io/SystemOut.java
@@ -0,0 +1,8 @@
+package lessons.les_16.io;
+
+public class SystemOut {
+    public static void main(String[] args) {
+        System.out.println("OUT");
+        System.out.println("ERROR");
+    }
+}
