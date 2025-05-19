1147e74ae4fb1a763cfddb3f376e6a2c890fd197
Stanislav Yashnov <s.yashnov@gmail.com>
2025-05-19 22:28:14 +0300

    les11

---
+++ JavaPro/lecture/src/lessons/les_18/ex/Ex5.java
@@ -0,0 +1,8 @@
+package lessons.les_18.ex;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("TOMATO");
+        System.out.println(sb.reverse().toString().replace('O', 'A'));
+    }
+}
