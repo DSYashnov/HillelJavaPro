68dc00fbabe434bc87c992866120a647741c72f5
Stanislav Yashnov <s.yashnov@gmail.com>
2025-05-20 23:49:13 +0300

    les11

---
+++ JavaPro/lecture/src/lessons/les_18/ex/Ex6.java
@@ -0,0 +1,12 @@
+package lessons.les_18.ex;
+
+import java.util.Arrays;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        String[] str = new String[10];
+        str[0] = "FIRST";
+        System.out.println(Arrays.asList(str).size());
+        System.out.println(Arrays.asList(str));
+    }
+}
