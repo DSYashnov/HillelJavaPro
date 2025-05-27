4b7694a655ac9d19f4e729b60ffffc050208a93e
Stanislav Yashnov <s.yashnov@gmail.com>
2025-05-28 00:08:30 +0300

    les11

--- mvnProjects/testTaskJdbc/src/main/java/service/DBService.java
+++ mvnProjects/testTaskJdbc/src/main/java/service/DBService.java
@@ -1,8 +1,16 @@
 package service;
 
+import dto.Student;
+
 import java.sql.SQLException;
 import java.util.List;
 
 public interface DBService {
     List<String> getGroupList() throws SQLException;
+
+    List<Student> getGroupLisByGroup(String groupNumber) throws SQLException;
+
+    void createStudent(Student student) throws SQLException;
+
+    void deleteStudent(int id) throws SQLException;
 }
