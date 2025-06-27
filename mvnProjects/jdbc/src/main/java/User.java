f74b50e92ea9b9137239ee34c13180910db143f5
Stanislav Yashnov <s.yashnov@gmail.com>
2025-06-27 23:09:17 +0300

    les11

---
+++ mvnProjects/jdbc/src/main/java/User.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class User {
+    private int id;
+    private String last_name;
+    private String first_name;
+    private int age;
+    private String phone;
+    private String email;
+}
