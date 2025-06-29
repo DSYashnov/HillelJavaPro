be66081b709324f44bfc1bf5ce34cbe402b5c388
Stanislav Yashnov <s.yashnov@gmail.com>
2025-06-29 17:34:34 +0300

    les11

---
+++ mvnProjects/spark-java/src/main/java/com/sparkjava/UserException.java
@@ -0,0 +1,12 @@
+package com.sparkjava;
+
+public class UserException extends Exception{
+
+    public UserException(){
+        super();
+    }
+
+    public UserException (String message) {
+        super(message);
+    }
+}
